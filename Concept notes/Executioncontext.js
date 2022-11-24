b4068e4a4190cdbd547d74e23ef1f8dcb633f279
sandhyar1 <93361725+sandhyar1@users.noreply.github.com>
2022-11-24 12:21:52 +0530

    Add files via upload

---
+++ Concept notes/Executioncontext.js
@@ -0,0 +1,16 @@
+//Everything in js happens in execution context.
+//How this EC looks like.
+// it is like a big box it has 2 box in it there are two componennts in it where as 
+// first is memory 
+
+
+// Memory component(Vraiable Environment)
+// where variavles,functions,key value pairs.it is also known as variable environment.
+// it is an environment where all sort of functions and varibles are StereoPannerNode.apply a sthe key value pairs.Memory
+
+// Code component(Thread of execution)
+// this is the place where a whole ocde run one line at a time.
+
+// 2.Js is synchronus,Single threaded language
+// .synchrnous-it excecuted one command at atime in a specific order
+// it executed one line by line.
